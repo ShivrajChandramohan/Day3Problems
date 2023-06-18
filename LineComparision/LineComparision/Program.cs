97733a05bd110a23bdfc3f8472ecf1aa0db32e00
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-06-18 18:51:26 +0530

    [Shivraj] Add : LineComparisionProblems

---
+++ LineComparision/LineComparision/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Problems");
+
+            LineBased.Calculatelength();
+
+            Equality.Equal();
+
+            EndPoint.Greaterorless();
+        }
+    }
+}
