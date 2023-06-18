42bdf481880f47eda682a7234b5a9dc3912c089e
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-06-18 18:35:13 +0530

    [Shivraj] Add : EmployeeWageProblems

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Class");
+
+            UC1.CheckAttendance();
+
+            UC2.employeewage();
+
+            UC3.Parttimewage();
+
+            UC4.switchcase();
+
+            UC5.workingdays();
+
+            UC6.workinghrs();
+            Console.ReadLine();
+        }
+    }
+}
